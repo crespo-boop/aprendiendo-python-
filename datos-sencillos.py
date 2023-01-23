64b9aef09cb553ae6a8210699bcef959ba435b85
Jean carlos Crespo pachay <e1317047445@live.uleam.edu.ec>
2023-01-23 09:12:07 -0500

    commit en repositorio

---
+++ datos-sencillos.py
@@ -0,0 +1,22 @@
+' entre los datos que maneja python esta los textos'
+'estos se pueden imprimir con el print o comentar con unas comillas simples'
+print('hola mundito me gustan las gordas')
+' ejemplos'
+"asi"
+'asi'
+'''
+tus datos son
+nombre:juan
+apellido reynoso
+
+'''
+
+"""hola
+piola
+aja
+"""
+
+ab=1
+a=1.1
+'van con mayusculas',True
+'van con mayusculas',False
