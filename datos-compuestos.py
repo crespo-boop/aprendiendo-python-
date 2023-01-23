2c381fc73fe6ef525baf7fcb851bc3c52c1d2484
Jean carlos Crespo pachay <e1317047445@live.uleam.edu.ec>
2023-01-23 09:25:28 -0500

    variables compuestas

---
+++ datos-compuestos.py
@@ -0,0 +1,8 @@
+'una variable es un dato que se guarda en la memoria de nuetsro programa, las variables se declaran y se definen'
+' pueden ser:'
+a=2
+b=3
+c=a+b
+print(c)
+nombre="felip"
+print(nombre)
