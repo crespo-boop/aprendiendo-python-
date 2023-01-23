3f107f354bf1b29105daf3744429e7c8d9aad610
Jean carlos Crespo pachay <e1317047445@live.uleam.edu.ec>
2023-01-23 09:49:17 -0500

    avance

--- datos-compuestos.py
+++ datos-compuestos.py
@@ -6,3 +6,15 @@ c=a+b
 print(c)
 nombre="felip"
 print(nombre)
+
+num=1
+num+=3
+num-=0.5
+print(num)
+nombre=True
+"se usa f string para convertir cualquier dato en string"
+bienvenida=f"hola {nombre} te culeo?"
+"se usa la operacion dle para eliminar variables"
+"del nombre"
+print("culeo" not in bienvenida)
+print("culeo" in bienvenida)
